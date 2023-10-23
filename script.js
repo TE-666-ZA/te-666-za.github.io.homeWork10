b4e8c586a8a56ecb585638658d7a871168b06e9d
Kenan Iusubovi <te666za@gmail.com>
2023-10-23 10:04:37 +0200

    done

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const numByIndex = (array, index ) => {
 
 const arr = [4,5,6,9,8,7,5,1,74,52]
 
+console.log(arr + ":  original array")
+
 
 console.log(numByIndex(arr,8) + " = log for function(arr, 8)")
 
